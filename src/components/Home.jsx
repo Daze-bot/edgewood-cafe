27930200a5dad83882e35ab0c3ddb52d5c86a156
Code4Daze <jevon.fillette@gmail.com>
2023-11-16 05:37:23 -0500

    Start home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,15 @@
+import Drawing from '../assets/imgs/drawing.jpg';
+
 const Home = () => {
   return (
     <div className="home">
       <h1>Edgewood Cafe</h1>
+      <img src={Drawing} alt='Logo'></img>
+      <p>1730 Yardley Langhorne Rd, Yardley, PA</p>
+      <hr></hr>
+      <p>(215) 321-0500</p>
+      <hr></hr>
+      <span>Open every day for Breakfast and Lunch from 7am - 3pm</span>
     </div>
   )
 };
